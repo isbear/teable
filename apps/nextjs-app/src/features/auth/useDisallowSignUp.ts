c34c7cf105442190f106facf54d0c455a12325d5
Boris <boris2code@outlook.com>
2025-07-31 17:35:08 +0800

    feat: splite delete user service

--- apps/nextjs-app/src/features/auth/useDisallowSignUp.ts
+++ apps/nextjs-app/src/features/auth/useDisallowSignUp.ts
@@ -1,12 +1,23 @@
 import { useQuery } from '@tanstack/react-query';
 import { getPublicSetting } from '@teable/openapi';
 import { ReactQueryKeys } from '@teable/sdk/config';
+import { useRouter } from 'next/router';
+import { useMemo } from 'react';
 
 export const useDisallowSignUp = () => {
   const { data: setting } = useQuery({
     queryKey: ReactQueryKeys.getPublicSetting(),
     queryFn: () => getPublicSetting().then(({ data }) => data),
   });
+  const router = useRouter();
+  const redirect = decodeURIComponent((router.query.redirect as string) || '');
+  const isInvitation = useMemo(() => {
+    return (
+      redirect.includes('invitation') &&
+      redirect.includes('invitationCode') &&
+      redirect.includes('invitationId')
+    );
+  }, [redirect]);
   const { disallowSignUp } = setting ?? {};
-  return disallowSignUp;
+  return disallowSignUp && !isInvitation;
 };
