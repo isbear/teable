57ddd4a61538b027f80165a195cd3c91cd81d20e
nichenqin <nichenqin@hotmail.com>
2025-08-04 21:10:24 +0800

    refactor: remove dbGenerated option from formula fields and related logic

--- apps/nestjs-backend/test/field-converting.e2e-spec.ts
+++ apps/nestjs-backend/test/field-converting.e2e-spec.ts
@@ -225,7 +225,6 @@ describe('OpenAPI Freely perform column transformations (e2e)', () => {
 
       const { newField } = await expectUpdate(table1, sourceFieldRo, newFieldRo);
       expect(newField.options).toEqual({
-        dbGenerated: true,
         expression: '"text"',
       });
     });
